8c27bd49b2799ef17c2dc8c1fccf071c60751f9c
Goska <36965568+goskanowak@users.noreply.github.com>
2018-03-31 02:20:30 +0200

    Update scripts

--- scripts.js
+++ scripts.js
@@ -1,15 +1,15 @@
+var triangle1Area = getTriangleArea(1, 15);
+var triangle2Area = getTriangleArea(10, -15);
+var triangle3Area = getTriangleArea(18, 75);
 
 function getTriangleArea(a, h) {
     if (a >= 0 && h >= 0) {
         return a*h/2;       
     } else {
-      return "Nieprawidłowe dane";
+        return "Nieprawidłowe dane";
     }   
 }
 
-var triangle1Area = getTriangleArea(1, 15);
-var triangle2Area = getTriangleArea(10, -15);
-var triangle3Area = getTriangleArea(18, 75);
 console.log(triangle1Area);
 console.log(triangle2Area);
 console.log(triangle3Area);
